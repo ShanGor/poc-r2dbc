106af22dc5603277b8d2248169e9255e12fcbbb7
Samuel Chan <emschan@live.cn>
2024-04-15 23:24:06 +0800

    test flexible SQL in the r2dbc

--- demo-r2dbc/src/main/java/com/example/demo/controller/IndexController.java
+++ demo-r2dbc/src/main/java/com/example/demo/controller/IndexController.java
@@ -3,20 +3,26 @@ package com.example.demo.controller;
 import com.example.demo.data.entity.UserEntity;
 import com.example.demo.data.repository.UserRepository;
 import com.example.demo.exception.AppException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.r2dbc.postgresql.codec.Json;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import jakarta.annotation.Resource;
-import java.nio.file.ProviderNotFoundException;
 
 @RestController
 @Slf4j
 public class IndexController {
     @Resource
     UserRepository userRepo;
+
+    ObjectMapper om = new ObjectMapper();
 
     private static final AppException NOT_FOUND_EXCEPTION = new AppException(400, "Not found the id!");
     @GetMapping("/users/{userId}")
@@ -32,6 +38,33 @@ public class IndexController {
             System.out.printf("%s - %s %n", k, v.get(0));
         });
         return "Hello, %s".formatted(hello.message);
+    }
+
+    @Resource
+    R2dbcEntityTemplate template;
+
+    @GetMapping("/test-sql")
+    public Flux testSql() {
+        // the element is a mutable map, so we do not need to copy it.
+        return template.getDatabaseClient().sql("select * from test").fetch().all().map(o -> {
+            o.entrySet().forEach(e -> {
+                if (e.getValue() instanceof io.r2dbc.postgresql.codec.Json) {
+                    String value = ((Json) e.getValue()).asString();
+                    try {
+                        if (value.startsWith("{")){
+                            o.put(e.getKey(), om.readValue(value, java.util.Map.class));
+                        } else if (value.startsWith("[")){
+                            o.put(e.getKey(), om.readValue(value, java.util.List.class));
+                        } else {
+                            o.put(e.getKey(), value);
+                        }
+                    } catch (JsonProcessingException ex) {
+                        o.put(e.getKey(), value);
+                    }
+                }
+            });
+            return o;
+        });
     }
 
     @Data
